884c6b53186d8110c41f8c8856e6ac25b4dda963
Samirk60 <samir.sinha01@gmail.com>
2026-02-17 17:11:44 +0530

    update

--- index.js
+++ index.js
@@ -36,7 +36,11 @@ mongoose.connect(process.env.MONGODB_URI)
 //   res.send('server is running with mongoDB');
 // });
 
- const PORT = process.env.PORT || 5000;
+//  const PORT = process.env.PORT || 5000;
+//  app.listen(PORT, () => {   console.log(`Server is running on port ${PORT}`);
+// });
+
+ const PORT = process.env.PORT || 3000;
  app.listen(PORT, () => {   console.log(`Server is running on port ${PORT}`);
 });
 
